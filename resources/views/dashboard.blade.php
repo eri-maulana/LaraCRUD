e2f24b3b8d580b45b7f9891321bafe049ef9ddf9
eri-maulana <maulanabouble11@gmail.com>
2024-02-26 17:59:28 +0700

    1. Persiapan Project

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="card bg-white">
+                <div class="card-body text-center">
+                  <form action="" method="post">
+                    @csrf
+                    <textarea name="" class="textarea textarea-bordered w-full" id="" rows="3" placeholder="apa yang kamu pikirkan ?"></textarea>
+                    <input type="submit" value="Tweet" class="btn btn-primary  ">
+                  </form>
+                </div>
+              </div>
+        </div>
+    </div>
+</x-app-layout>
